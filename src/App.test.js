12aabca083ecb5a29d6950a93ab379cd2d996df5
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-01-28 13:15:43 +0100

    working on react testing library

--- src/App.test.js
+++ src/App.test.js
@@ -3,4 +3,11 @@ import App from './App';
 
 test('input should be initially empty', () => {
   render(<App />);
+  const emailInputElement = screen.getByRole('textbox');
+  const passwordInputElement = screen.getByLabelText('Password');
+  const confirmPasswordInputElement =
+    screen.getByLabelText(/confirm password/i);
+  expect(emailInputElement.value).toBe('');
+  expect(passwordInputElement.value).toBe('');
+  expect(confirmPasswordInputElement.value).toBe('');
 });
